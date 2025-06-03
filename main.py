52f1ca4e2180857b9e6f81e64179160d0711d598
genagena <chernyackov.eugeny@yandex.ru>
2025-06-03 16:11:05 +0300

    linreg with gradient descent

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from models.firstman import AnaliticLinReg
+from models.firstman import GradLinReg
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
-from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
 
 reg = AnaliticLinReg()
 data = pd.read_csv('data/processed/Sleep/sleep_mark1.csv')
@@ -11,11 +13,24 @@ X = data.drop(columns=['Sleep Duration'])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-reg.fit(X_train, y_train)
-y_pred = reg.predict(X_test)
+scaler = StandardScaler()
+X_train = pd.DataFrame(scaler.fit_transform(X_train))
+X_test = pd.DataFrame(scaler.transform(X_test))
 
-mae = mean_absolute_error(y_test, y_pred)
-print(f"MAE: {mae:.4f}")
+# ------------------
+
+reg.fit(X_train, y_train)
+y_pred1 = reg.predict(X_test)
+mse1 = mean_squared_error(y_test, y_pred1)
+
+# -------------------
+
+gradreg = GradLinReg(0.1, 100)
+gradreg.fit(X_train, y_train)
+y_pred2 = gradreg.predict(X_test)
+mse2 = mean_squared_error(y_test, y_pred2)
+
+print(round(mse1, 2), round(mse2, 2))
 
 
 
--- models/firstman.py
+++ models/firstman.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
 
+# метод наименьших квадратов точное решение
 class AnaliticLinReg():
     def __init__(self):
-        self.w = 0
+        pass
 
     def fit(self, X, y):
         X = X.to_numpy()
@@ -20,5 +21,38 @@ class AnaliticLinReg():
         X = np.hstack((ones_col, X))
 
         return X @ self.w
+
+# мнк приближенные методы(град спуски)
+class GradLinReg():
+    def __init__(self, lr, s):
+        self.lr = lr
+        self.s = s
+
+    def fit(self, X, y):
+        X = X.to_numpy()
+        ones_col = np.ones((X.shape[0], 1))
+        X = np.hstack((ones_col, X))
+        y = y.to_numpy().reshape(-1)
+
+        self.D = X.shape[1]
+        self.N = X.shape[0]
+        self.w = np.random.normal(size=(self.D,))
+
+        for i in range(self.s):
+            self.w = self.w - self.lr * self.calc_grad(X, y)
+
+        return self
+
+    def predict(self, X):
+        X = X.to_numpy()
+        ones_col = np.ones((X.shape[0], 1))
+        X = np.hstack((ones_col, X))
+
+        return X @ self.w
+
+    def calc_grad(self, X, y):
+        grad = (2 / self.N) * X.T @ (X @ self.w - y)
+        return grad
+
 
 # l1, l2 reg; stoch_des; learning_rate; cross-val; data
