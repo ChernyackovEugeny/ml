3c4cba4f401d501fb03532c04b2bb1ef0fcf6525
genagena <zenacernakov7@gmail.com>
2025-06-05 06:44:54 +0300

    check

--- preprocessing/IRIS_pre-processing.py
+++ preprocessing/IRIS_pre-processing.py
@@ -1,6 +1,4 @@
 import pandas as pd
-df = pd.read_csv('../data/raw/Iris.csv', index_col='Id')
+data = pd.read_csv('../data/raw/Iris.csv', index_col='Id')
 
-
-
-print(df['Species'].unique())
+print(data['Species'].unique())
--- usage/linclass.py
+++ usage/linclass.py
@@ -6,16 +6,25 @@ from models.linear_classification import LinClass
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.svm import LinearSVC
+# support vector classificator
 
 data = pd.read_csv('../data/processed/Person/person_mark1.csv')
 
 X = data.drop(columns=['Personality'])
 y = data['Personality']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, train_size=0.8, stratify=y)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, train_size=0.8)
-
-lincl = LinClass(0.1, 0.1, 10000)
+lincl = LinClass(0.1, 0.01, 100000)
 lincl.fit(X_train, y_train)
 y_pred = lincl.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
-print(accuracy)
+
+# ------------------
+skclass = LinearSVC(C=1.0, max_iter=10000)
+skclass.fit(X_train, y_train)
+sky_pred = skclass.predict(X_test)
+skaccuracy = accuracy_score(y_test, sky_pred)
+
+print(accuracy, skaccuracy)
+
